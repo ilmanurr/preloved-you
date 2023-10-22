29cc85b11977a88999bc46e1220033d90480cf09
haniza45 <124548866+haniza45@users.noreply.github.com>
2023-10-22 14:47:02 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
-// Toggle class active utk hamburger menu
+// Toggle class active untuk hamburger menu
 const isiNavbar = document.querySelector('.isinavbar');
-// ketika hamburger menu diklik
+// Ketika hamburger menu diklik
 document.querySelector('#hamburger-menu').onclick = () => {
     isiNavbar.classList.toggle('active');
 };
 
-// Toggle class active utk search form
+// Toggle class active untuk search form
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
-// ketika
+// Ketika search button diklik
 document.querySelector('#search-button').onclick = (e) => {
     searchForm.classList.toggle('active');
-    searchBox.focis();
+    searchBox.focus();
     e.preventDefault();
 };
 
-// Toggle class active utk login form
+// Toggle class active untuk login form
 const loginForm = document.querySelector('.login-form');
-// ketika login diklik
+// Ketika profil menu diklik
 document.querySelector('#profil-menu').onclick = (e) => {
     loginForm.classList.toggle('active');
-    loginBox.focis();
+    loginForm.focus();
     e.preventDefault();
 };
+
+// Menutup navigasi jika diklik di luar navigasi
+document.body.addEventListener('click', (e) => {
+    // Cek apakah yang diklik bukan bagian dari navigasi atau elemen-elemen yang dikecualikan
+    if (!e.target.closest('.isinavbar') && 
+        !e.target.closest('#hamburger-menu') && 
+        !e.target.closest('.search-form') && 
+        !e.target.closest('#search-button') && 
+        !e.target.closest('.login-form') && 
+        !e.target.closest('#profil-menu')) {
+        // Tutup navigasi (hilangkan class active)
+        isiNavbar.classList.remove('active');
+        searchForm.classList.remove('active');
+        loginForm.classList.remove('active');
+    }
+});
